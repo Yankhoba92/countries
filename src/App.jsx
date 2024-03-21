cb418f0c49f50f1205aae7c899995416bc79b144
Yankhoba <bagayoko.yan@gmail.com>
2024-03-21 09:54:11 +0100

    Gestion de mes Routes pour mes composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import CountryInfo from "./components/CountryInfo/CountryInfo";
+import Allcountries from "./components/allCountries/Allcountries";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className="header">
+        <div className="container">
+          <h5>Le Monde ou rien</h5>
+        </div>
+      </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Allcountries />} />
+          <Route path="/country/:countryName" element={<CountryInfo />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
